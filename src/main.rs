a0a5e9a94db722b9e00993587128f51af325c8fa
NoobProgramer6918 <bartek.szklarski@outlook.com>
2025-11-24 21:16:26 +0100

    changed .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 fn print_helpmsg() {
+    // Prints a help message
     println!("rs2lua - rust to lua compiler\n");
     println!("Help:");
     println!("  Usage: rs2lua -f FILE(s) -o LUA(Optional)\n");
@@ -9,17 +10,14 @@ fn print_helpmsg() {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let valid_args: Vec<String> = [
-        String::from("-f"),
-        String::from("--help")
-    ].to_vec();
+    let valid_args: Vec<String> = [String::from("-f"), String::from("--help")].to_vec();
 
     if args.iter().any(|i| valid_args.contains(i)) {
         // Check for file(s) or other arguments
 
-        if args.iter().any(|i| i == "--help") {print_helpmsg();} 
-
-
+        if args.iter().any(|i| i == "--help") {
+            print_helpmsg();
+        }
     } else {
         print_helpmsg(); // print help message when no arguments found
     }
